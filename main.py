21937360a62b281fc47c3b1eeb6f3b3e01ac80c7
Gunnar Tweeton <gtweeton@gmail.com>
2025-10-31 14:13:59 -0500

    setup pygame clock and 60 fps

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from constants import *
 def main():
     # initialize pygame
     pygame.init()
-    
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -26,6 +30,12 @@ def main():
 
             # --- Update the display ---
             pygame.display.flip()
+
+            # It will pause the game loop until 1/60th of a 
+            # second has passed
+            clock.tick(60)
+            dt = (clock.get_time() / 1000)
+            
 
 
 if __name__ == "__main__":
