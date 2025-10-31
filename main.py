6786460dfda13bbbebb5664686193e83caf0a033
Gunnar Tweeton <gtweeton@gmail.com>
2025-10-31 13:55:48 -0500

    game loop started

--- main.py
+++ main.py
@@ -2,9 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    # initialize pygame
+    pygame.init()
+    
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # use pygame's display.set_mode() to get a new GUI window:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_running = True
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # --- Fill the screen with black ---
+            screen.fill((0, 0, 0))
+
+            # --- Update the display ---
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
